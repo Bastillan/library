d8c6e71b6fb0763c8c1b3b5ed0621cc4903a2bf3
jkedzier <jedrzej.kedzierski.stud@pw.edu.pl>
2024-11-21 11:00:21 +0100

    Added registration, login and user roles

---
+++ zad1/MvcLibrary/MvcLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcLibrary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<MvcLibrary.Models.ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ zad1/MvcLibrary/MvcLibrary/Program.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MvcLibrary.Data;
+using MvcLibrary.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<ApplicationUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+using(var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    var roles = new[] { "Librarian", "Reader" };
+
+    foreach (var role in roles)
+    {
+        if(!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+    string username = "Librarian";
+    string password = "Zad1@mvc";
+    string email = "mvclibrary@mvc.pl";
+    string firstname = "librarian";
+    string lastname = "librarian";
+    
+    if (await userManager.FindByNameAsync(username) == null)
+    {
+        var user = new ApplicationUser();
+        user.UserName = username;
+        user.Email = email;
+        user.FirstName = firstname;
+        user.LastName = lastname;
+        await userManager.CreateAsync(user, password);
+        await userManager.AddToRoleAsync(user, "Librarian");
+    }
+}
+
+app.Run();

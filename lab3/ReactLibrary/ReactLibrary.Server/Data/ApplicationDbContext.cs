ff6092480f364c0515a5d19889f71436c6547fd4
jkedzier <jedrzej.kedzierski.stud@pw.edu.pl>
2024-12-28 16:44:58 +0100

    ApplicationUser

---
+++ lab3/ReactLibrary/ReactLibrary.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ReactLibrary.Server.Models;
+
+namespace ReactLibrary.Server.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ReactLibrary.Server.Models.ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        { }
+    }
+}

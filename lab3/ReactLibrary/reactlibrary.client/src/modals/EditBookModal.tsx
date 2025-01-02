965da7de7e4309d07ef7743ae5a5fbf0cd3c5b68
jkedzier <jedrzej.kedzierski.stud@pw.edu.pl>
2025-01-03 00:51:53 +0100

    Correction of modal title

--- lab3/ReactLibrary/reactlibrary.client/src/modals/EditBookModal.tsx
+++ lab3/ReactLibrary/reactlibrary.client/src/modals/EditBookModal.tsx
@@ -122,7 +122,7 @@ const EditBookModal = ({ modalId, bookId, onBookEdited }: EditBookModalProps) =>
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title" id={`modalTitle${modalId}`}>Add new book</h5>
+                        <h5 className="modal-title" id={`modalTitle${modalId}`}>Edit book</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
